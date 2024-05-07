882007ed11c2e0f466ca4baad6ba79ec552875ce
Rahul Das <das206053@gmail.com>
2024-05-07 15:51:53 +0530

    routing-done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,26 @@
+import {  BrowserRouter, Route, Routes } from "react-router-dom";
+import  {Signin}  from "./pages/Signin";
+import  {Signup}  from "../src/pages/Signup";
+import  {SendMoney}  from "./pages/SendMoney";
+import { Dashboard } from "./pages/Dashboard";
 
 function App() {
-
   return (
+
     <div>
-        Hello world
+      <BrowserRouter>
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard/>} />
+
+        <Route path="/signin" element={<Signin />}></Route>
+
+        <Route path="/signup" element={<Signup />}></Route>
+
+        <Route path="/send" element={<SendMoney />}></Route>
+      </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
