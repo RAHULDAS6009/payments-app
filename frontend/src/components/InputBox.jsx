214cfc3f50534d3251f0e80308b2c155f35b6516
Rahul-Das <das206053@gmail.com>
2024-05-15 13:44:10 +0530

    basic integration

--- frontend/src/components/InputBox.jsx
+++ frontend/src/components/InputBox.jsx
@@ -1,6 +1,12 @@
-export const InputBox=({label})=>{
-    return <div>
-        <div className="text-black font-medium my-1.5">{label}</div>
-        <input className="w-full h-8 rounded-md border border-slate-400 focus:outline-none focus:ring focus:ring-green-200" type="text" />
+export const InputBox = ({ label, onChange,type }) => {
+  return (
+    <div>
+      <div className="text-black font-medium my-1.5">{label}</div>
+      <input
+        className="w-full h-8 rounded-md border border-slate-400 focus:outline-none focus:ring focus:ring-green-200"
+        type={type}
+        onChange={onChange}
+      />
     </div>
-}
+  );
+};
